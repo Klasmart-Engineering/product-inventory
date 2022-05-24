f31d4eec66fb433bd5246b93dbcb89f32a6f9be2
andreas.marneris <andreas.marneris@kidsloop.live>
2022-05-24 13:17:18 +0100

    fix: renamed variable set to a new name specific for product inventory

--- tfe-ws-token-varset.tf
+++ tfe-ws-token-varset.tf
@@ -1,5 +1,5 @@
 resource "tfe_variable_set" "tfe-ws-token-service" {
-  name          = "tfe-ws-token-service"
+  name          = "tfe-ws-token-product-inventory"
   description   = "This variable set contains a Terraform Cloud token for self-configuration of workspaces in the \"services-meta\" projects."
   organization  = local.tfe_organization
   workspace_ids = [
